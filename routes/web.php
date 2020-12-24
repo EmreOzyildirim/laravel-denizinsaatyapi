1a2da26f2b276d2c12e991d81694941ef94655de
Emre Ozyildirim <emreozyildirim@outlook.com>
2020-12-25 01:33:40 +0300

    routes updated -1:33

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/welcome', 'PageController@index');
+Route::get('/welcome', 'App\Http\Controllers\PageController@index');
 
-Route::get('/show', 'PageController@show');
+Route::get('/show', 'App\Http\Controllers\PageController@show');
 
 Route::get('/', function () {
     return view('welcome');
